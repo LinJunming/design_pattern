665636aa15b39ce9b2231b55e9e38c1f2e4880b5
ljm <1367149105@qq.com>
2019-09-25 23:39:23 +0800

    增加工厂方法模式的测试代码

--- design_pattern/main.cpp
+++ design_pattern/main.cpp
@@ -1,5 +1,11 @@
+#ifdef TEST_SIMPLE_FACTORY
 #include "factory/factory.h"
+#else
+#include "factory_method/range_extender_factory.h"
+#include "factory_method/wireless_router_factory.h"
+#endif
 
+#ifdef TEST_SIMPLE_FACTORY
 // 工厂模式的测试函数
 void TestFactory()
 {
@@ -16,9 +22,34 @@ void TestFactory()
 	// pause
 	getchar();
 }
+#else
+void TestFactoryMethod()
+{
+	// 工厂方法模式在使用上和简单工厂的不同之处：
+	// 不需要传“产品类型”这个参数；但上层就需要知道具体工厂类的定义。
+	RangeExtenderFactory* range_extender_factory = new RangeExtenderFactory();
+	WirelessRouterFactory* wireless_router_factory = new WirelessRouterFactory();
 
+
+	RangeExtender *range_extender = (RangeExtender*)range_extender_factory->CreateProduct();
+	WirelessRouter *wireless_router = (WirelessRouter*)wireless_router_factory->CreateProduct();
+
+	if (range_extender != nullptr) {
+		range_extender->Info();
+	}
+	if (wireless_router != nullptr) {
+		wireless_router->Info();
+	}
+	// pause
+	getchar();
+}
+#endif
 int main()
 {
+#ifdef TEST_SIMPLE_FACTORY
 	TestFactory();
+#else
+	TestFactoryMethod();
+#endif
 	return 0;
 }
